91ba3116870b8b7b22e988b44cf65d3b141e7309
dvela-2023095 <dvela-2023095@kinal.edu.gt>
2025-08-29 14:03:38 -0600

    Se corrigen errores y se mejora el back

--- config/app.js
+++ config/app.js
@@ -6,13 +6,19 @@ import authRoutes from '../src/auth/auth.routes.js'
 import levelRoutes from '../src/level/level.routes.js'
 import questionRoutes from '../src/question/question.routes.js'
 import progressRoutes from '../src/progress/progress.routes.js'
-
-const configs =(app)=>{
+import {limiter} from '../middlewares/rate.limit.js'
+const configs = (app)=>{
     app.use(express.json())
     app.use(express.urlencoded({extended:true}))
     app.use(cors())
-    app.use(helmet())
+    app.use(helmet({
+        crossOriginResourcePolicy: { policy: 'cross-origin' },
+      })
+    )
     app.use(morgan('dev'))
+    app.use(limiter)
+
+    app.use('/uploads', cors(), express.static('uploads'))
 }
 
 const routes =(app)=>{
@@ -20,6 +26,7 @@ const routes =(app)=>{
     app.use('/v1/levels', levelRoutes)
     app.use('/v1/questions',questionRoutes)
     app.use('/v1/progress', progressRoutes)
+    app.use("/upload", express.static("upload"));
 }
 
 export const initServer = ()=>{
